9d766f66cc47dfdf5a0c0c7fbfc64b5c961abcae
Darth Vader <danieltarancon@gmail.com>
2022-08-15 17:59:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
-from sqlalchemy import create_engine()
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+
+
+
+engine = create_engine('sqlite:///inventory.db', echo = False)
+Base = declarative_base()
+
+
+
+
+
+
+
+
+
