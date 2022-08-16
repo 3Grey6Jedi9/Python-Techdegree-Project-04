6ad148837b5904bf0a290f28dd727f4e84122f8d
Darth Vader <danieltarancon@gmail.com>
2022-08-16 13:26:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, Date
 from sqlalchemy.ext.declarative import declarative_base
 
 
@@ -7,6 +7,15 @@ from sqlalchemy.ext.declarative import declarative_base
 engine = create_engine('sqlite:///inventory.db', echo = False)
 Base = declarative_base()
 
+
+class Product(Base):
+    __tablename__ = 'products'
+
+    product_id = Column(Integer, Primary_key=True)
+    product_name = Column('Name', String)
+    product_quantity = Column('Quantity', Integer)
+    product_price = Column('Price', Integer)
+    date_update = Column('Updated', Date)
 
 
 
