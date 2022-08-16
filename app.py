59d6c8c27a29a6164da359748ef2558b11c3fba6
Darth Vader <danieltarancon@gmail.com>
2022-08-16 18:59:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,50 @@
 from models import Base, session, Product, engine
 
 
+
+
+def menu():
+    while True:
+        print("""
+        \nPRODUCTS
+        \r1) Add product
+        \r2) View all
+        \r3) Search for product
+        \r4) Product Analysis 
+        \r5) Exit
+        """)
+        choice = input('What would you like to do? ')
+        if choice in ['1','2','3','4','5']:
+            return choice
+        else:
+            input(''' \nPlease choose one of the options above.
+            \rA number from 1-5.
+            \rPress enter to try again.''')
+
+
+
+def app():
+    app_running = True
+    while app_running:
+        choice = menu()
+        if choice == '1':
+            pass
+        elif choice == '2':
+            pass
+        elif choice == '3':
+            pass
+        elif choice == '4':
+            pass
+        else:
+            print('GOODBYE')
+            app_running = False
+
+
+
 if __name__ == '__main__':
     Base.metadata.create_all(engine)
+    app()
+
 
 
 
