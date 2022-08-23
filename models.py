f06b9018e3d357f036affb751e70c961c40c5b29
Darth Vader <danieltarancon@gmail.com>
2022-08-23 13:59:05 +0200

    Update models.py

--- models.py
+++ models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, Integer, String, Date
+from sqlalchemy import create_engine, Column, Integer, String, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -16,7 +16,7 @@ class Product(Base):
     product_name = Column('Name', String)
     product_quantity = Column('Quantity', Integer)
     product_price = Column('Price', Integer)
-    date_update = Column('Updated', Date)
+    date_update = Column('Updated', DateTime)
 
     def __repr__(self):
         return f'Name: {self.product_name}; Quantity: {self.product_quantity}; Price: {self.product_price}; Updated: {self.date_update};'
